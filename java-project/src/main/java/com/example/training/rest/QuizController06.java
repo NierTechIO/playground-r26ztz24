ed0413f8422a207f8474a4e558e451212f62aaa8
Nierrrrrrr <Nierrrrrrr@users.noreply.github.com>
2019-10-22 16:57:05 +0800

    Update QuizController06.java

--- java-project/src/main/java/com/example/training/rest/QuizController06.java
+++ java-project/src/main/java/com/example/training/rest/QuizController06.java
@@ -8,6 +8,8 @@ public class QuizController06 {
         3. retrieve file by filePathName, begins with "./files" folder
           * for example, if filePathName = "/data/hello.txt", final file path will be "./files/data/hello.txt"
         4. return file bytes
-        5. Only files under ./files/ can be downloaded
+        5. Only files under ./files/ can be downloaded, if not, return HTTP CODE 403
+        6. If folder or file not found, return HTTP CODE 404
+        7. If filePathName is a folder, return HTTP CODE 404
      */
 }
