968c8d8c78e1b4513f7fd1648b316058364d525e
Nierrrrrrr <Nierrrrrrr@users.noreply.github.com>
2019-10-22 17:28:39 +0800

    Update QuizController06Test.java

--- java-project/src/test/java/com/example/training/rest/QuizController06Test.java
+++ java-project/src/test/java/com/example/training/rest/QuizController06Test.java
@@ -1,9 +1,13 @@
 package com.example.training.rest;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,8 +16,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class QuizController06Test extends ControllerMockMvcTestBase {
     @Test
     public void restQuiz06() throws Exception {
-        // get real file bytes from
-        byte[] fileBytes = Files.readAllBytes(Paths.get("./", "files", "data", "hello.txt"));
+        // write bytes to file
+        Path filePath = Paths.get("./", "files", "data", "hello.txt");
+        filePath.toFile().getParentFile().mkdirs();
+        byte[] fileBytes = "Hello, download file!!".getBytes();
+        try (FileOutputStream fos = new FileOutputStream(filePath.toFile())) {
+            fos.write(fileBytes);
+        }
 
         mockMvc.perform(get("/restQuiz06")
                 .param("filePathName", "/data/hello.txt"))
@@ -22,6 +31,9 @@ public class QuizController06Test extends ControllerMockMvcTestBase {
                     byte[] content = mvcResult.getResponse().getContentAsByteArray();
                     Assert.assertArrayEquals(fileBytes, content);
                 });
+
+        // remove test file
+        FileUtils.deleteDirectory(new File("./files"));
 
         // path traversal
         mockMvc.perform(get("/restQuiz06")
