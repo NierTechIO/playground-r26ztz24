b830011e0138b08782ecd8e46f703de9abf87aee
Nierrrrrrr <Nierrrrrrr@users.noreply.github.com>
2019-10-22 16:47:59 +0800

    Create QuizController06.java

---
+++ java-project/src/main/java/com/example/training/rest/QuizController06.java
@@ -0,0 +1,13 @@
+package com.example.training.rest;
+
+public class QuizController06 {
+    /* TODO:
+        1. add GET REST API with url "/restQuiz06"
+        2. 1 GET parameter with name "filePathName"
+          * filePathName example: "/data/hello.txt"
+        3. retrieve file by filePathName, begins with "./files" folder
+          * for example, if filePathName = "/data/hello.txt", final file path will be "./files/data/hello.txt"
+        4. return file bytes
+        5. Only files under ./files/ can be downloaded
+     */
+}
