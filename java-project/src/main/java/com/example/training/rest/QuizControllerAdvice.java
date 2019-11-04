adb9a98f39839ec4ca288b5f7da6b9e08035581a
Nierrrrrrr <Nierrrrrrr@users.noreply.github.com>
2019-11-04 10:34:17 +0800

    Create QuizControllerAdvice.java

---
+++ java-project/src/main/java/com/example/training/rest/QuizControllerAdvice.java
@@ -0,0 +1,9 @@
+package com.example.training.rest;
+
+/* TODO: Complete this ControllerAdvice
+    if there's AccessDeniedException, return 403,
+    if there's NullPointerException, return 201,
+    if other Exception, return 400
+ */
+public class QuizControllerAdvice {
+}
